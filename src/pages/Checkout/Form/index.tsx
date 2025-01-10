4bea3849d3c4df1797321f1b606230c407214227
Gustav Uwe <gustavuwe.123@gmail.com>
2025-01-10 12:21:48 -0300

    changes in checkout page

--- src/pages/Checkout/Form/index.tsx
+++ src/pages/Checkout/Form/index.tsx
@@ -1,72 +1,78 @@
+import { AdressText, CheckoutFormContainer, LeftForm, LeftFormTitle } from "./styles";
+
 export function CheckoutForm() {
   return (
     <>
-      <div>
-        <div>
-          <h1>Complete seu Pedido</h1>
-          <form>
-            <h1>Endereço de entrega</h1>
-            <p>Informe o endereço onde deseja receber seu pedido</p>
+      <CheckoutFormContainer>
+        <LeftForm>
+          <div>
+            <LeftFormTitle>Complete seu Pedido</LeftFormTitle>
+            <form>
+              <AdressText>
+                <h1>Endereço de entrega</h1>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </AdressText>
 
-            <input type="text" placeholder="CEP" />
-            <input type="text" placeholder="Rua" />
-            <div>
-              <input type="text" placeholder="Número" />
-              <input type="text" placeholder="Complemento" />
-            </div>
-            <div>
-              <input type="text" placeholder="Bairro" />
-              <input type="text" placeholder="Cidade" />
-              <input type="text" placeholder="UF" />
-            </div>
-          </form>
-        </div>
+              <input type="text" placeholder="CEP" />
+              <input type="text" placeholder="Rua" />
+              <div>
+                <input type="text" placeholder="Número" />
+                <input type="text" placeholder="Complemento" />
+              </div>
+              <div>
+                <input type="text" placeholder="Bairro" />
+                <input type="text" placeholder="Cidade" />
+                <input type="text" placeholder="UF" />
+              </div>
+            </form>
+          </div>
 
-        <div>
-          <form>
-            <h1>Pagamento</h1>
-            <p>
-              O pagamento é feito na entrega. Escolha a forma que deseja pagar
-            </p>
+          <div>
+            <form>
+              <h1>Pagamento</h1>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
 
-            <div>
-              <button>CARTÃO DE CRÉDITO</button>
-              <button>CARTÃO DE DÉBITO</button>
-              <button>DINHEIRO</button>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div>
-        Cafés selecionados
+              <div>
+                <button>CARTÃO DE CRÉDITO</button>
+                <button>CARTÃO DE DÉBITO</button>
+                <button>DINHEIRO</button>
+              </div>
+            </form>
+          </div>
+        </LeftForm>
         <div>
+          Cafés selecionados
           <div>
-            <h1>Expresso Tradicional</h1>
             <div>
-              <button>- 1 +</button>
-              <button>Remover</button>
+              <h1>Expresso Tradicional</h1>
+              <div>
+                <button>- 1 +</button>
+                <button>Remover</button>
+              </div>
+            </div>
+            <div>
+              <p>R$ 9,90</p>
             </div>
           </div>
           <div>
-            <p>R$ 9,90</p>
+            <div>
+              <p>Total de Itens</p>
+              <p>R$ 29,70</p>
+            </div>
+            <div>
+              <p>Entrega</p>
+              <p>R$ 3,50</p>
+            </div>
+            <div>
+              <h1>Total</h1>
+              <h1>R$ 33,20</h1>
+            </div>
+            <button>CONFIRMAR PEDIDO</button>
           </div>
         </div>
-        <div>
-          <div>
-            <p>Total de Itens</p>
-            <p>R$ 29,70</p>
-          </div>
-          <div>
-            <p>Entrega</p>
-            <p>R$ 3,50</p>
-          </div>
-          <div>
-            <h1>Total</h1>
-            <h1>R$ 33,20</h1>
-          </div>
-          <button>CONFIRMAR PEDIDO</button>
-        </div>
-      </div>
+      </CheckoutFormContainer>
     </>
   );
 }
--- src/pages/Checkout/Form/styles.ts
+++ src/pages/Checkout/Form/styles.ts
@@ -1,2 +1,58 @@
 import styled from "styled-components";
 
+export const CheckoutFormContainer = styled.section`
+  height: calc(100vh - 6.5rem);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  gap: 3rem;
+`;
+
+export const LeftForm = styled.div`
+  max-width: 640px;
+  display: flex;
+  flex-direction: column;
+
+  form {
+    padding: 2.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+    background-color: ${(props) => props.theme["base-card"]};
+    border-radius: 6px;
+
+    input {
+    }
+  }
+`;
+
+export const LeftFormTitle = styled.h1`
+  font-size: 1.125rem;
+  font-family: "Baloo 2", sans-serif;
+  color: ${(props) => props.theme["base-subtitle"]};
+  margin-bottom: 1rem;
+`;
+
+export const AdressText = styled.div`
+  line-height: 1.5;
+
+  h1 {
+    font-size: 1rem;
+    font-family: "Roboto", sans-serif;
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 0.875rem;
+    font-family: "Roboto", sans-serif;
+    color: ${(props) => props.theme["base-text"]};
+    font-weight: 400;
+  }
+
+
+`;
