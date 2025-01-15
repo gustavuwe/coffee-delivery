a199002a668e8d99d919916dc4a31ca4dfa95414
Gustav Uwe <gustavuwe.123@gmail.com>
2025-01-15 18:38:15 -0300

    feat: increase real data to cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,39 @@
-import logo from "../../assets/logo.svg"
-import { MapPin, ShoppingCart } from "phosphor-react"
-import { HeaderContainer } from "./styles"
+import logo from "../../assets/logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
+import { HeaderContainer } from "./styles";
+import { useContext } from "react";
+import { CoffeesContext } from "../../contexts/CoffeesContext";
+import { Link } from "react-router-dom";
 
 export function Header() {
+  const { coffees } = useContext(CoffeesContext);
+
+  const totalCoffees = coffees.reduce(
+    (total, coffee) => total + coffee.quantity,
+    0
+  );
+
   return (
     <HeaderContainer>
-      <img src={logo} alt="" />
+      <Link to="/home">
+        <img src={logo} alt="" />
+      </Link>
       <nav>
-        <button className="location-button"><MapPin size={24} color="purple" weight="fill" />Porto Alegre, RS</button>
-        <button className="cart-button">
-        <ShoppingCart size={24} color="#C47F17" weight="fill" />
-        <div className="cart-items-quantity">
-          <p>3</p>
-        </div>
+        <button className="location-button">
+          <MapPin size={24} color="purple" weight="fill" />
+          Porto Alegre, RS
         </button>
+        <Link to="/checkout">
+          <button className="cart-button">
+            <ShoppingCart size={24} color="#C47F17" weight="fill" />
+            {totalCoffees > 0 ? (
+              <div className="cart-items-quantity">
+                <p>{totalCoffees}</p>
+              </div>
+            ) : null}
+          </button>
+        </Link>
       </nav>
     </HeaderContainer>
-  )
+  );
 }
