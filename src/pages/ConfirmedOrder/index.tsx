883403506f381266bce786d8ef271e8f82082f30
Gustav Uwe <gustavuwe.123@gmail.com>
2025-01-17 18:09:56 -0300

    add base styling to confirmed order page

--- src/pages/ConfirmedOrder/index.tsx
+++ src/pages/ConfirmedOrder/index.tsx
@@ -4,19 +4,33 @@ export function ConfirmedOrder() {
   return (
     <ConfirmedOrderContainer>
       <div>
-        <div>
+        <div className="text-container">
           <h1>Uhu! Pedido confirmado</h1>
           <p>Agora é só aguardar que logo o café chegará até você</p>
         </div>
-        <div>
+        <div className="delivery-container">
           <div>
             <div>
               <img src="src/assets/coffee-delivery.png" alt="" />
-              <div>
-                <p>
-                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
-                </p>
-                <p>Farrapos - Porto Alegre, RS</p>
+              <div className="delivery-content-container">
+                <div>
+                  <p>
+                    Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  </p>
+                  <p>Farrapos - Porto Alegre, RS</p>
+                </div>
+                <div>
+                  <p>
+                    Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  </p>
+                  <p>Farrapos - Porto Alegre, RS</p>
+                </div>
+                <div>
+                  <p>
+                    Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  </p>
+                  <p>Farrapos - Porto Alegre, RS</p>
+                </div>
               </div>
             </div>
           </div>
--- src/pages/ConfirmedOrder/styles.ts
+++ src/pages/ConfirmedOrder/styles.ts
@@ -1,4 +1,40 @@
 import styled from "styled-components";
 
 export const ConfirmedOrderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 6.5rem;
+
+  .text-container {
+    h1 {
+      font-size: 2rem;
+      font-family: "Baloo 2", sans-serif;
+      color: ${(props) => props.theme["yellow-dark"]};
+      font-weight: bold;
+    }
+
+    p {
+      font-size: 1.25rem;
+      font-family: "Roboto", sans-serif;
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+  }
+
+  .delivery-container {
+    margin-top: 2rem;
+    width: 526px;
+    padding: 40px;
+    border-top-left-radius: 6px;
+    border-bottom-right-radius: 6px;
+    border-top-right-radius: 36px;
+    border-bottom-left-radius: 36px;
+    border: 1px solid ${(props) => props.theme["base-button"]};
+
+    .delivery-content-container {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+    }
+  }
 `;
